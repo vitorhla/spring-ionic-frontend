9d59c762e473febd552eb6e9ecc90bc477a36346
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-03-23 20:28:35 -0300

    Lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: String = 'HomePage';
+  rootPage: string = 'HomePage';
 
   pages: Array<{title: string, component: string}>;
 
