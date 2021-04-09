1d30dc3e9b12942858a6e6a30ea544c30a416f31
VitorAndrade0721 <vitorhenriquealt@gmail.com>
2021-04-08 21:46:23 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -18,6 +18,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
 
   constructor(public navCtrl: NavController,
@@ -50,17 +51,21 @@ total(){
   return this.cartService.total();
 }
 
+
 back(){
   this.navCtrl.setRoot('CartPage');
 }
 
+home(){
+this.navCtrl.setRoot('CategoriasPage');
 
+}
 
 checkout(){
   this.pedidoService.insert(this.pedido).subscribe (response => {
     this.cartService.createOrClearCart();
-    
-    console.log(response.headers.get('location'));
+
+    this.codpedido= this.extractId(response.headers.get('location'));
 
   },
   error =>{
@@ -69,5 +74,9 @@ checkout(){
 
     }
   });
+}
+private extractId(location : string) :  string{
+  let position = location.lastIndexOf('/');
+  return location.substring(position +1, location.length);
 }
 }
