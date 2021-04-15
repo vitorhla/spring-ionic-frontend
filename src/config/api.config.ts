f490256e1226e889a8fa6f02359569704bd75ae2
vitorhla <vitorhenriquealt@gmail.com>
2021-04-15 20:49:58 -0300

    Testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
 
-baseUrl:"http://localhost:8080",
+baseUrl:"https://spring-ionic-vitorhla.herokuapp.com",
 bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/vitor.andrade"
 
 
